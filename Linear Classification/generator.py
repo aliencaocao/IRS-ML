87b892bc7883c7c1fb2fbae4b50c02d53fc8264c
Billy Cao <aliencaocao@gmail.com>
2021-04-16 18:37:09 +0800

    Started on linear classification

---
+++ Linear Classification/generator.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+import random
+
+def gen(n):
+    theta = (2, 3)
+    theta_nought = 2000
+    while True:
+        X = tf.random.uniform((n,2), minval=-1000., maxval=1500., dtype=tf.float32)
+        y = tf.math.sign(tf.reduce_sum(X*theta, axis=-1) - theta_nought)
+        yield X, y
