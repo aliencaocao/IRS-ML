7c63fbd268b6f7ef75ab6ccf99e681dd93a123c0
Billy Cao <aliencaocao@gmail.com>
2021-12-10 17:01:19 +0800

    Sequence and time series forecasting codes

---
+++ Sequences/Conv1D.py
@@ -0,0 +1,124 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+print(tf.__version__)
+
+
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+
+def trend(time, slope=0):
+    return slope * time
+
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+series = trend(time, 0.1)
+baseline = 10
+amplitude = 40
+slope = 0.05
+noise_level = 5
+
+# Create the series
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+# Update with noise
+series += noise(time, noise_level, seed=42)
+
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+window_size = 60
+batch_size = 64
+shuffle_buffer_size = 1000
+
+
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    series = tf.expand_dims(series, axis=-1)  # expand from 1D to 2D for LSTM
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
+tf.keras.backend.clear_session()
+tf.random.set_seed(69)
+np.random.seed(69)
+
+tf.keras.backend.clear_session()
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv1D(filters=32, kernel_size=5, strides=1, padding="causal", activation="relu", input_shape=[None, 1]),
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    tf.keras.layers.LSTM(32, return_sequences=True),
+    tf.keras.layers.Dense(30, activation="relu"),
+    tf.keras.layers.Dense(15, activation="relu"),
+    tf.keras.layers.Dense(1),
+    tf.keras.layers.Lambda(lambda x: x * 100)
+])
+
+model.compile(loss="mse", optimizer=tf.keras.optimizers.SGD(learning_rate=1e-5, momentum=0.9), metrics=["mae"])
+history = model.fit(dataset, epochs=400, verbose=1)
+forecast = []
+results = []
+for time in range(len(series) - window_size):
+    forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
+
+forecast = forecast[split_time - window_size:]
+results = np.array(forecast)[:, 0, 0]
+
+plt.figure(figsize=(10, 6))
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, results)
+tf.keras.metrics.mean_absolute_error(x_valid, results).numpy()
+plt.show()
+
+# -----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+# -----------------------------------------------------------
+mae = history.history['mae']
+loss = history.history['loss']
+
+epochs = range(len(loss))  # Get number of epochs
+
+# ------------------------------------------------
+# Plot MAE and Loss
+# ------------------------------------------------
+plt.plot(epochs, mae, 'r')
+plt.plot(epochs, loss, 'b')
+plt.title('MAE and Loss')
+plt.xlabel("Epochs")
+plt.ylabel("Accuracy")
+plt.legend(["MAE", "Loss"])
+
+plt.figure()
+plt.show()
---
+++ Sequences/DNN.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+print(tf.__version__)
+
+
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+
+
+def trend(time, slope=0):
+    return slope * time
+
+
+def seasonal_pattern(season_time):
+    """Just an arbitrary pattern, you can change it if you wish"""
+    return np.where(season_time < 0.4,
+                    np.cos(season_time * 2 * np.pi),
+                    1 / np.exp(3 * season_time))
+
+
+def seasonality(time, period, amplitude=1, phase=0):
+    """Repeats the same pattern at each period"""
+    season_time = ((time + phase) % period) / period
+    return amplitude * seasonal_pattern(season_time)
+
+
+def noise(time, noise_level=1, seed=None):
+    rnd = np.random.RandomState(seed)
+    return rnd.randn(len(time)) * noise_level
+
+
+time = np.arange(4 * 365 + 1, dtype="float32")
+baseline = 10
+series = trend(time, 0.1)
+baseline = 10
+amplitude = 40
+slope = 0.05
+noise_level = 5
+
+# Create the series
+series = baseline + trend(time, slope) + seasonality(time, period=365, amplitude=amplitude)
+# Update with noise
+series += noise(time, noise_level, seed=42)
+
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+
+
+# windowed dataset below requires tf.keras.layers.Lambda(lambda x: tf.expand_dims(x, axis=-1), input_shape=[None]) to expand to 3D for use in RNNs
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+    dataset = tf.data.Dataset.from_tensor_slices(series)
+    dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+    dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+    dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+    dataset = dataset.batch(batch_size).prefetch(1)
+    return dataset
+
+
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+print(dataset)
+l0 = tf.keras.layers.Dense(16, input_shape=[window_size])
+l1 = tf.keras.layers.Dense(32, input_shape=[window_size])
+l2 = tf.keras.layers.Dense(1, input_shape=[window_size])
+model = tf.keras.models.Sequential([l0, l1, l2])
+
+model.compile(loss="mae", optimizer=tf.keras.optimizers.SGD(learning_rate=7e-6))
+model.fit(dataset, epochs=500, verbose=1)
+
+print("Layer weights {}".format(l0.get_weights()))
+
+forecast = []
+
+for time in range(len(series) - window_size):
+    forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
+
+forecast = forecast[split_time - window_size:]
+results = np.array(forecast)[:, 0, 0]
+
+plt.figure(figsize=(10, 6))
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, results)
+plt.show()
